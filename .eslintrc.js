76e0454bbc87d09dc62dd85b7486ba7d90720ebf
Safelg Preciado <safelgpreciado@gmail.com>
2023-10-29 21:09:36 -0700

    I guess it works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,35 @@
 module.exports = {
   env: {
-    node: true,
-    commonjs: true,
+    browser: true,
     es6: true,
+    'jest/globals': true,
+    node: true,
   },
-  extends: 'eslint:recommended',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  settings: {
+    react: {
+      version: 'detect', // or specify your React version
+    },
   },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
-    sourceType: module,
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'error',
+    'react/prop-types': 0,
+    'no-console': 'off',
   },
 }
